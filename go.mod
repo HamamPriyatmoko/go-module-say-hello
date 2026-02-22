e6b40892aedfa4f5621c7d740e5cfa12369d6add
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2026-02-23 01:33:39 +0700

    update say hello with paramter

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/HamamPriyatmoko/go-module-say-hello
+module github.com/HamamPriyatmoko/go-module-say-hello/v2
 // Jika ingin membuat module nama modulenya harus gunakan url dari repository
 // dimana module itu disimpan digithub dan jangan tambahkan .git ataupun https-nya
 go 1.25.6
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,7 @@
 package module_say_hello
 
-func SayHello() string {
-	return "Hello World"
+// Untuk menghindari adanya fitu yang tidak kompatible pada versi module terbaru/terlama
+// lebih baik mengganti nama modulenya dan dengan tambahan versionnya 
+func SayHello(name string) string {
+	return "Hello " + name
 }
